b89485bbecb788a4504409f3f30b46b61eb28913
onion3030 <76668994+bluebirdsoft@users.noreply.github.com>
2021-07-22 21:09:07 +0800

    Update install_bluebirdfire.sh

--- install_bluebirdfire.sh
+++ install_bluebirdfire.sh
@@ -130,7 +130,7 @@ function start_ufw() {
 function get_package(){
 
 rm /var/www/*
-https://github.com/bluebirdsoft/firewall/releases/download/1.1/bluebird_firewall_v1.0.tar.gz -O /opt/bluebird_firewall/bluebird_firewall_v1.0.tar.gz
+wget https://github.com/bluebirdsoft/firewall/releases/download/1.1/bluebird_firewall_v1.0.tar.gz -O /opt/bluebird_firewall/bluebird_firewall_v1.0.tar.gz
 }
 
 function unzip_bluebird_firewall(){
